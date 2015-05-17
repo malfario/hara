68a128b27c8e6266fa88df4312621c1f5106eb8f
Chris Zheng <z@caudate.me>
2015-05-17 23:55:42 +0530

    Fixing issue #5 and fix for delegate

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/zcaudate/hara"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-midje-doc "0.0.23"]
