e4f0f01ae945dfa5d711c1cf6589e73da888d397
Chris Zheng <z@caudate.me>
2015-05-31 00:58:50 +0530

    midje-doc v2 compatible file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject im.chit/hara "2.1.12-SNAPSHOT"
-  :description "General purpose utilities library"
+  :description "code patterns and utilities"
   :url "https://github.com/zcaudate/hara"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
@@ -8,12 +8,55 @@
                    :plugins [[lein-midje "3.1.3"]
                              [lein-midje-doc "0.0.23"]
                              [lein-repack "0.2.10"]]}}
-  :documentation {:files {"docs/index"
-                        {:input "test/midje_doc/hara/outline.clj"
-                         :title "hara"
-                         :sub-title "General Purpose Utilities Library"
-                         :author "Chris Zheng"
-                         :email  "z@caudate.me"}}}
+  :documentation {:type   :portfolio
+                  :name   "hara"
+                  :output "docs/index.html"
+                  :description "code patterns and utilities"
+                  :tracking "UA-31320512-2"
+                  :owners [{:name    "Chris Zheng"
+                            :email   "z@caudate.me"
+                            :website "http://z.caudate.me"}]
+                  :files {
+                          ;"home"
+                          ;{:input "test/midje_doc/guides/home.clj"
+                          ; :template "full"
+                          ; :title "home"}
+                          "orientation"
+                          {:input "test/midje_doc/hara/orientation.clj"
+                           :title "hara in small pieces"
+                           :link {:auto-number false}}
+                          ;"api"
+                          ;{:input "test/midje_doc/hara/outline.clj"
+                          ; :title "api reference"}
+                          ;"hara.component"
+                          ;{:input "test/midje_doc/guides/hara_component.clj"
+                          ; :title "hara.component - composable systems"}
+                          ;"hara.ova"
+                          ;{:input "test/midje_doc/guides/hara_ova.clj"
+                          ; :title "hara.ova - reactive array"}
+                          ;"hara.io.watch"
+                          ;{:input "test/midje_doc/guides/hara_io_watch.clj"
+                          ; :title "hara.io.watch - file watcher"}
+                          ;"hara.io.timer"
+                          ;{:input "test/midje_doc/guides/hara_io_timer.clj"
+                          ; :title "hara.io.scheduler - task scheduler"}
+                          }
+                :html {:logo "hara.png"
+                       :theme "clean"
+                       :home "home",
+                       :navigation ["quickstart"
+                                    ["guides" ["hara.component" 
+                                               "hara.ova" 
+                                               "hara.io.watch" 
+                                               "hara.io.timer"]]
+                                    {:link "api", :text "api"}
+                                    {:link "https://gitter.im/zcaudate/hara", 
+                                     :text "support"}
+                                    {:link "https://www.github.com/zcaudate/hara", 
+                                     :text "source"}]}
+                :link {:auto-tag    true
+                       :auto-number true}}
+                  
   :global-vars {*warn-on-reflection* true}
   :repack [{:type :clojure
             :levels 2
---
+++ test/midje_doc/hara/orientation.clj
@@ -0,0 +1,8 @@
+(ns midje-doc.hara.orientation)
+
+[[:article {:authors [{:name "Chris Zheng" 
+                       :email "z@caudate.me" 
+                       :website "http://z.caudate.me"}]
+            :link {:auto-number #{:equation :figure :code}}}]]
+
+[[:chapter {:title "Introduction"}]]
