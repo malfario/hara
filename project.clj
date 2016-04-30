f74c8d29bb84d1d1e46c52819c3d15ed95e7fe28
Chris Zheng <z@caudate.me>
2016-04-30 13:43:19 +0300

    v2.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject im.chit/hara "2.3.0"
+(defproject im.chit/hara "2.3.1"
   :description "patterns and utilities"
   :url "https://github.com/zcaudate/hara"
   :license {:name "The MIT License"
